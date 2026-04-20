01995f05266d4b5b0ecea9da122f7af38dc8b437
rof1yev <rofiyevdilshod@gmail.com>
2026-04-20 12:19:59 +0500

    fix constants variable

--- lib/constants.ts
+++ lib/constants.ts
@@ -43,7 +43,7 @@ export const adminSideBarLinks = [
 ];
 
 export const FIELD_NAMES = {
-  fullname: "Full name",
+  fullName: "Full name",
   email: "Email",
   universityId: "University ID Number",
   password: "Password",
@@ -51,7 +51,7 @@ export const FIELD_NAMES = {
 };
 
 export const FIELD_TYPES = {
-  fullname: "text",
+  fullName: "text",
   email: "email",
   universityId: "number",
   password: "password",
