95b5434d8a43313109ddbe4f1c6ae5682e09d5f2
rof1yev <rofiyevdilshod@gmail.com>
2026-04-21 11:09:03 +0500

    implement borrow functionality fixed

--- lib/actions/book.ts
+++ lib/actions/book.ts
@@ -9,47 +9,49 @@ export const borrowBook = async (params: BookBorrowParams) => {
   const { userId, bookId } = params;
 
   try {
-    await db.transaction(async (tx) => {
-      const [book] = await tx
-        .select({ availableCopies: books.availableCopies })
-        .from(books)
-        .where(eq(books.id, bookId))
-        .limit(1);
+    const [book] = await db
+      .select({ availableCopies: books.availableCopies })
+      .from(books)
+      .where(eq(books.id, bookId))
+      .limit(1);
 
-      if (!book || book.availableCopies <= 0) {
-        throw new Error("Book unavailable");
-      }
+    if (!book || book.availableCopies <= 0)
+      return {
+        success: false,
+        message: "Sorry, this book is currently unavailable.",
+      };
 
-      const [existingBorrow] = await tx
-        .select()
-        .from(borrowRecords)
-        .where(
-          and(
-            eq(borrowRecords.userId, userId),
-            eq(borrowRecords.bookId, bookId),
-            eq(borrowRecords.status, "BORROWED"),
-          ),
-        )
-        .limit(1);
+    const [existingBorrow] = await db
+      .select()
+      .from(borrowRecords)
+      .where(
+        and(
+          eq(borrowRecords.userId, userId),
+          eq(borrowRecords.bookId, bookId),
+          eq(borrowRecords.status, "BORROWED"),
+        ),
+      )
+      .limit(1);
 
-      if (existingBorrow) {
-        throw new Error("Already borrowed");
-      }
+    if (existingBorrow)
+      return {
+        success: false,
+        message: "You have already borrowed this book.",
+      };
 
-      const dueDate = dayjs().add(7, "day").toDate().toDateString();
+    const dueDate = dayjs().add(7, "day").toDate().toDateString();
 
-      await tx.insert(borrowRecords).values({
-        userId,
-        bookId,
-        dueDate,
-        status: "BORROWED",
-      });
-
-      await tx
-        .update(books)
-        .set({ availableCopies: book.availableCopies - 1 })
-        .where(eq(books.id, bookId));
+    await db.insert(borrowRecords).values({
+      userId,
+      bookId,
+      dueDate,
+      status: "BORROWED",
     });
+
+    await db
+      .update(books)
+      .set({ availableCopies: book.availableCopies - 1 })
+      .where(eq(books.id, bookId));
 
     return {
       success: true,
